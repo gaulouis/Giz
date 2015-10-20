eb7f5bfbf29a121f5e9f52ed096e91e4eed498e9
gaulouis <gaulouis.fr@gmail.com>
2015-10-20 16:50:35 +0200

    Create Storage/Session.php

---
+++ library/Giz/Storage/Session.php
@@ -0,0 +1,66 @@
+<?php
+
+use Zend\Authentication\Storage\StorageInterface;
+
+class Giz\Storage\Session implements StorageInterface
+{
+    /**
+     * Returns true if and only if storage is empty
+     *
+     * @throws \Zend\Authentication\Exception\ExceptionInterface
+     *               If it is impossible to
+     *               determine whether storage is empty
+     * @return boolean
+     */
+    public function isEmpty()
+    {
+        /**
+         * @todo implementation
+         */
+    }
+
+    /**
+     * Returns the contents of storage
+     *
+     * Behavior is undefined when storage is empty.
+     *
+     * @throws \Zend\Authentication\Exception\ExceptionInterface
+     *               If reading contents from storage is impossible
+     * @return mixed
+     */
+    public function read()
+    {
+        /**
+         * @todo implementation
+         */
+    }
+
+    /**
+     * Writes $contents to storage
+     *
+     * @param  mixed $contents
+     * @throws \Zend\Authentication\Exception\ExceptionInterface
+     *               If writing $contents to storage is impossible
+     * @return void
+     */
+    public function write($contents)
+    {
+        /**
+         * @todo implementation
+         */
+    }
+
+    /**
+     * Clears contents from storage
+     *
+     * @throws \Zend\Authentication\Exception\ExceptionInterface
+     *               If clearing contents from storage is impossible
+     * @return void
+     */
+    public function clear()
+    {
+        /**
+         * @todo implementation
+         */
+    }
+}
